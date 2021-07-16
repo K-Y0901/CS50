e5ec5ada85d0ee7ae5701e606f1eac5f407b9928
EC2 Default User <yoshiki100901@icloud.com>
2021-07-16 16:29:56 +0000

    pset2

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -1,1 +1,23 @@
 #include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./ceasar key");
+        return 1;
+    }
+    
+    int key = atoi(argv[1]);
+    
+    string text = get_string("plaintext: ");
+    printf("ciphertext: ");
+    for (int i = 0; text[i] != "\0"; i++)
+    {
+        
+    }
+}
