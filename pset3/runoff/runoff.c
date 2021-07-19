a65ea330819d7b4b936fc697372fb11f27923c1f
EC2 Default User <yoshiki100901@icloud.com>
2021-07-19 12:26:39 +0000

    pset3

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -193,12 +193,26 @@ int find_min(void)
 bool is_tie(int min)
 {
     // TODO
-    return false;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (!candidates[i].eliminated && candidates[i].votes != min)
+        {
+            return false;
+        }
+        return true;
+    }
 }
 
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
     // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == min)
+        {
+            candidates[i].eliminated = true;
+        }
+    }
     return;
 }
