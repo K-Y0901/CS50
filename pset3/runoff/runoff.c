6f94d482b60a2aa345f8897b218f88762445c4a8
EC2 Default User <yoshiki100901@icloud.com>
2021-07-19 11:54:21 +0000

    pset3

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -129,6 +129,14 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, candidates[i].name) == 0)
+        {
+            preferences[voter][rank] = i;
+            return true;
+        }
+    }
     return false;
 }
 
