58d3c3a111bf3d6ab46635ba9e0e0ed0825e15ac
EC2 Default User <yoshiki100901@icloud.com>
2021-07-27 07:04:21 +0000

    pset4

--- pset4/filter/more/filter/helpers.c
+++ pset4/filter/more/filter/helpers.c
@@ -19,6 +19,15 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            RGBTRIPLE pxa = image[i][j];
+            image[i][j] = image[i][width - 1 -j];
+            image[i][width - 1 - j] = pxa;
+        }
+    }
     return;
 }
 
