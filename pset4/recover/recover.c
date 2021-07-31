c9ced733e9fed21edd91e4967d2830db0d4b91bf
EC2 Default User <yoshiki100901@icloud.com>
2021-07-31 15:32:15 +0000

    pset4

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -6,5 +6,19 @@
  
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: ./recover image\n");
+        return 1;
+    }
+    
+    FILE *file = fopen(argc[1], "r");
+    
+    if (FILE == NULL)
+    {
+        fprintf(stderr, "Your image cannot be opened. \n");
+        return 1;
+    }
+    
+    
 }
