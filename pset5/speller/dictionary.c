98b595c42273e5f7f5df1b13a717c79915f91e68
EC2 Default User <yoshiki100901@icloud.com>
2021-08-02 10:55:15 +0000

    pset6

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -1,6 +1,12 @@
 // Implements a dictionary's functionality
 
+#include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <ctype.h>
+
 
 #include "dictionary.h"
 
@@ -22,6 +28,17 @@ node *table[N];
 bool check(const char *word)
 {
     // TODO
+    int index = hash(word);
+    cursor = table[index]->next;
+    while (cursor != NULL)
+    {
+        int result = strcasecmp(cursor->word, word);
+        if (result == 0)
+        {
+            return true;
+        }
+        sursor = sursor->next;
+    }
     return false;
 }
 
@@ -29,26 +46,79 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO
-    return 0;
+    int index = (char)tolower(word[0] - "a");
+    return index;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
     // TODO
-    return false;
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Your dictionary text file cannot be opened.\n");
+        return false;
+    }
+    
+    // Initialize a head of linked lists
+    for (int i = 0; i < N; i++)
+    {
+        node *item = malloc(sizeof(node));
+        if (item == NULL)
+        {
+            fprintf(stderr, "Can not allocate memory.\n");
+            return false;
+        }
+
+        item->next = NULL;
+        table[i] = item;
+    }
+    
+    // Read a file and put a data into linked lists
+    char buffer[LENGTH + 1];
+    int index = 0;
+    while (fscanf(file, "%s", buffer) != EOF)
+    {
+        node *item = malloc(sizeof(node));
+        if (item == NULL)
+        {
+            fprintf(stderr, "Can not allocate memory.\n");
+            return false;
+        }
+
+        strcpy(item->word, buffer);
+
+        index = hash(item->word);
+        item->next = table[index]->next;
+        table[index]->next = item;
+        count++;
+    }
+    fclose(file);
+
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
     // TODO
-    return 0;
+    return count;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
     // TODO
-    return false;
+    for (int i = 0; i < N; i++)
+    {
+        cursor = table[i];
+        while (cursor != NULL)
+        {
+            node *tmp = cursor;
+            cursor = cursor->next;
+            free(tmp);
+        }
+    }
+    return true;
 }
