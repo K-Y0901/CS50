781273f130990f37cc9fc32c0ef1bd2648d8a78c
EC2 Default User <yoshiki100901@icloud.com>
2021-08-05 17:59:09 +0000

    pset6

--- pset6/readabiilty/readability.py
+++ pset6/readabiilty/readability.py
@@ -3,4 +3,10 @@ from cs50 import get_string
 def is_word_end(c):
     return c == " "
 
+def is_sentence_end(c):
+    return c == "." or c == "!" or c == "?"
+    
+def is_letter(c):
+    return c.isalpha()
+    
 
