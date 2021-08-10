cce1b7acfc9f1ab6607abee6d3ccc2dfc2cd5348
EC2 Default User <yoshiki100901@icloud.com>
2021-08-10 16:32:06 +0000

    pset7

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -1,2 +1,13 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- SELECT receiver FROM phone_calls WHERE month = 7 AND day = 28 AND phone_number = (959) 555-4871;
+-- Theft of the CS50 duck took place at 10:15am at the Chamberlin Street courthouse
+-- The applicable lisence_plate of courthouse_security_logs is "11J91FW"
+-- "11J91FW" of courthouse_security_logs is Noah, so I think he is the theft
+-- Noah's passport number is 1095374669
+-- Noah's flight_id after 28th is 51
+-- Applicable destination_airport_id is No.8 whose name of city is Fiftyville
+-- Noah was seated on 6c
+-- Noah7s phone number is (959) 555-4871
+-- Noah' s id is 375525
+
